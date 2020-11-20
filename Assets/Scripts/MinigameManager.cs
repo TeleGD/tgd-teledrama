7ef9bba33e404e00ff53515e22a7a1e1c328013b
Stéphane HULOT <stephane.hulot@telecomnancy.eu>
2020-11-20 12:52:50 +0100

    Déclanchement des mini jeux

---
+++ Assets/Scripts/MinigameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinigameManager : MonoBehaviour
+{
+    public RectTransform contentHolder;
+    public GameObject[] games;
+
+    public void StartGame(int index)
+    {
+        contentHolder.parent.gameObject.SetActive(true);
+        Instantiate(games[index], contentHolder);
+    }
+
+    public void CloseGame()
+    {
+        if(contentHolder.childCount > 0)
+            Destroy(contentHolder.GetChild(0).gameObject);
+        contentHolder.parent.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/MinigameTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameTrigger : MonoBehaviour
+{
+    public int gameIndex;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == PlayerController.me.gameObject)
+            GameManager.instance.GetComponent<MinigameManager>().StartGame(gameIndex);
+    }
+}

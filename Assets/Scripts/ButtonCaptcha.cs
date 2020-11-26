f4241e60df5945f57ee42079dc88cbc19263b623
Lucas Gouin <lucas.gouin@telecomnancy.eu>
2020-11-26 14:37:36 +0100

    mini jeu Captcha

---
+++ Assets/Scripts/ButtonCaptcha.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonCaptcha : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int number;
+    public Button button;
+    public GameObject panel;
+    
+    void Start()
+    {
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(taskOnClick);
+    }
+
+    public void taskOnClick()
+    {
+        print(number.ToString() + " appuyé");
+        panel.gameObject.GetComponent<PanelCaptcha>().checkButton(number);
+    }
+}
---
+++ Assets/Scripts/PanelCaptcha.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+using UnityEngine.UI;
+using System;
+
+public class PanelCaptcha : MonoBehaviour
+{
+    public List<Captcha> captchas = new List<Captcha>();
+    private Captcha current;
+
+
+    public GameObject panelImage;
+
+    void Start()
+    {
+        restart();
+    }
+
+    public void checkButton(int button)
+    {
+        if (!current.getButton(button))
+        {
+            print("pas bon bouton");
+            restart();
+        }
+        else
+        {
+            print("bon bouton");
+            current.setButton(button);
+            if (current.checkList())
+            {
+                print("tous les boutons ont bien été appuyé");
+            }
+        }
+    }
+
+    private void restart()
+    {
+        System.Random random = new System.Random();
+        int numb = random.Next(0, captchas.Count);
+
+        current = captchas[numb];
+        current.reset();
+        panelImage.gameObject.GetComponent<Image>().sprite = current.Image;
+
+        
+    }
+}
+
+[Serializable]
+public class Captcha
+{
+    public Sprite Image;
+
+    public List<bool> goodButton = new List<bool>();
+
+    private List<bool> pushedButtons = new List<bool>();
+
+    public Captcha()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            pushedButtons.Add(false);
+        }
+    }
+
+    public void setButton(int index)
+    {
+        pushedButtons[index] = true;
+    }
+
+    public bool getButton(int index)
+    {
+        return (goodButton[index]);
+    }
+
+    public bool checkList()
+    {
+        for (int i = 0; i < pushedButtons.Count; i++)
+        {
+            if (pushedButtons[i] != goodButton[i]) return false;
+        }
+        return true;
+    }
+
+    public void reset()
+    {
+        for (int i = 0; i < pushedButtons.Count; i++)
+        {
+            pushedButtons[i] = false;
+        }
+    }
+}
